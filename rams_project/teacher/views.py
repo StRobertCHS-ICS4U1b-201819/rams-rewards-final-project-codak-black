70742127d881fde3bfaef2336b3b36a136289c6a
nataliet57 <nataliewytam@gmail.com>
2019-01-14 12:23:35 -0500

    Linking blog to home page

--- rams_project/teacher/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return render(request, 'teacher/home.html')
-
-def contact(request):
-    return render(request, 'teacher/basic.html',{'content':['If you '
-     'would like to contact me, please email me.','hskinsley@gmail.com']})
