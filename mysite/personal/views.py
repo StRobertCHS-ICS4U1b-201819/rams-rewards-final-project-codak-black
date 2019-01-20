cc0c7b4c94ca95d1c5b1c5e42bf1846ae6da0c30
nataliet57 <nataliewytam@gmail.com>
2019-01-20 00:35:03 -0500

    Adding text content to home page

--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -4,4 +4,7 @@ def index(request):
     return render(request, 'personal/home.html')
 
 def contact(request):
-    return render(request, 'personal/basic.html', {'content':['If you would like to contact me, please email me','hskinsley@gmail.com']})
+    return render(request, 'personal/basic.html', {
+        'content':['For general inquiries:','St. Robert Catholic High School', '8101 Leslie St., Thornhill L3T 7P4',
+                   'Phone: 905-889-4982', 'Attendance: 905-889-6149', 'Email: srh@ycdsb.ca ' ]})
+
