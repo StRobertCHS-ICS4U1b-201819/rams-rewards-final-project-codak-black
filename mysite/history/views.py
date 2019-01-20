2ba60a8d17a90014ac11917917839eccb77dfe3b
nataliet57 <nataliewytam@gmail.com>
2019-01-20 02:06:56 -0500

    Adding additional information for display

--- mysite/history/views.py
+++ mysite/history/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
 
+from .models import History
+
 def history(request):
     template_name = 'history/history.html'
+    queryset = History.objects.all( )
     context = {
-        "object_list":[12, 14, 232]
+        "object_list":queryset
     }
     return render(request, template_name, context)
